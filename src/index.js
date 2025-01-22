cddc4e85bf203b0701e19f41220d235712dd0ce3
tamdt5 <thanhtamdang103@gmail.com>
2025-01-22 18:06:23 +0700

    add route + controller

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { dirname} from 'path';
 import path from 'path';
 import { engine as handlebars } from 'express-handlebars';
 import * as sass from 'sass';
+import route from './routes/index.js';
+
 const app = express();
 const port = 3000
 const __filename = fileURLToPath(import.meta.url);
@@ -13,7 +15,7 @@ const __dirname = dirname(__filename);
 app.use(express.static(path.join(__dirname,'public')))
 
 // HTTP Logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine
 app.engine('hbs', handlebars({
@@ -22,15 +24,11 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
+app.use(express.urlencoded({ extended: false }))
+
 const result = await sass.compileAsync(path.join(__dirname,'resources/scss/app.scss'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
